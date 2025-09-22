e7e3a8f3d4e577f78def804978d77c5bbd14ce6e
desty <dimonmikryukov@gmail.com>
2025-09-22 21:48:01 +0400

    style: update header layout

--- frontend/src/components/header/page.tsx
+++ frontend/src/components/header/page.tsx
@@ -1,52 +1,39 @@
-"use client"
-import Link from "next/link"
-import Authentication from "@/components/auth/authentication"
-
+import Link from "next/link";
+import Authentication from "@/components/auth/authentication";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-center py-4 bg-white shadow-sm">
-      <div className="flex items-center px-80">
-        <nav className="justufy-center space-x-4">
-          <Link 
-            href="/news" 
-            className="text-sm font-semibold text-gray-700 hover:text-gray-800 transition-colors"
-          >
-            Новости
-          </Link>
-          <Link 
-            href="/statistics" 
-            className="text-sm font-semibold text-gray-700 hover:text-gray-800 transition-colors"
-          >
-            Статистика
-          </Link>
-          <Link 
-            href="/services" 
-            className="text-sm font-semibold text-gray-700 hover:text-gray-800 transition-colors"
-          >
-            Услуги
-          </Link>
-          <Link 
-            href="/contacts" 
-            className="text-sm font-semibold text-gray-700 hover:text-gray-800 transition-colors"
-          >
-            Контакты
-          </Link>
-          <Link 
-            href="/projects" 
-            className="text-sm font-semibold text-gray-700 hover:text-gray-800 transition-colors"
-          >
-            Проекты
-          </Link>
-          <Link 
-            href="/about" 
-            className="text-sm font-semibold text-gray-700 hover:text-gray-800 transition-colors"
-          >
-            О нас
-          </Link>
-        </nav>
+    <header className="flex items-center gap-3.5 mt-[3.125rem] bg-transparent shadow-sm absolute top-0 left-0 right-0 z-50 h-[12.5rem] md:h-[12.5rem]">
+      <div className="flex justify-between w-full max-w-[1920px] mx-auto px-[7.5rem] items-start">
+        <div className="flex flex-row h-full items-end text-white gap-2.5">
+          <div className="w-[12.5rem] h-[12.5rem] bg-gray-200"></div>
+          <p className="font-[400] text-[0.9375rem] leading-[1.25rem] tracking-[0%]">
+            Центр организации дорожного <br />
+            движения смоленской области
+          </p>
+        </div>
+        <div className="flex flex-1 items-center justify-between">
+          <nav className="flex items-center space-x-4">
+            {[
+              { href: "/news", label: "Новости" },
+              { href: "/statistics", label: "Статистика" },
+              { href: "/services", label: "Услуги" },
+              { href: "/contacts", label: "Контакты" },
+              { href: "/projects", label: "Проекты" },
+              { href: "/about", label: "О нас" },
+            ].map((link) => (
+              <Link
+                key={link.href}
+                href={link.href}
+                className="text-white font-semibold text-sm hover:text-[#62A744] transition-colors"
+              >
+                {link.label}
+              </Link>
+            ))}
+          </nav>
+          <Authentication />
+        </div>
       </div>
-        <Authentication/>
     </header>
-    )
+  );
 }
