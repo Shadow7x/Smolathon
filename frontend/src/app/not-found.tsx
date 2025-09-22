8af8248007c53526ce68aa9bf4dfc4ac157e9fa2
desty <dimonmikryukov@gmail.com>
2025-09-23 01:01:23 +0400

    feat: add notfound page

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center text-center">
+      <h1 className="text-6xl font-bold mb-4">404</h1>
+      <p className="text-xl mb-8">Страница не найдена</p>
+      <Link href="/" className="text-[#62a744] hover:underline">
+        Вернуться на главную
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
