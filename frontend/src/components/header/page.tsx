f69166b3b0debd2513ca28378dd66247ac94b6cf
HomnixMirn <vitalik.999f9cfe@gmail.com>
2025-09-22 21:24:09 +0400

    header

--- frontend/src/components/header/page.tsx
+++ frontend/src/components/header/page.tsx
@@ -5,50 +5,48 @@ import Authentication from "@/components/auth/authentication"
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between px-8 py-4 bg-white shadow-sm">
-      <div className="flex items-center space-x-12">
-    
-        <nav className="flex items-center space-x-6">
+    <header className="flex items-center justify-center py-4 bg-white shadow-sm">
+      <div className="flex items-center px-80">
+        <nav className="justufy-center space-x-4">
           <Link 
             href="/news" 
-            className="text-sm font-semibold text-gray-700 hover:text-blue-600 transition-colors"
+            className="text-sm font-semibold text-gray-700 hover:text-gray-800 transition-colors"
           >
             Новости
           </Link>
           <Link 
             href="/statistics" 
-            className="text-sm font-semibold text-gray-700 hover:text-blue-600 transition-colors"
+            className="text-sm font-semibold text-gray-700 hover:text-gray-800 transition-colors"
           >
             Статистика
           </Link>
           <Link 
             href="/services" 
-            className="text-sm font-semibold text-gray-700 hover:text-blue-600 transition-colors"
+            className="text-sm font-semibold text-gray-700 hover:text-gray-800 transition-colors"
           >
             Услуги
           </Link>
           <Link 
             href="/contacts" 
-            className="text-sm font-semibold text-gray-700 hover:text-blue-600 transition-colors"
+            className="text-sm font-semibold text-gray-700 hover:text-gray-800 transition-colors"
           >
             Контакты
           </Link>
           <Link 
             href="/projects" 
-            className="text-sm font-semibold text-gray-700 hover:text-blue-600 transition-colors"
+            className="text-sm font-semibold text-gray-700 hover:text-gray-800 transition-colors"
           >
             Проекты
           </Link>
           <Link 
             href="/about" 
-            className="text-sm font-semibold text-gray-700 hover:text-blue-600 transition-colors"
+            className="text-sm font-semibold text-gray-700 hover:text-gray-800 transition-colors"
           >
             О нас
           </Link>
         </nav>
       </div>
-    <Authentication/>
-
+        <Authentication/>
     </header>
     )
 }
