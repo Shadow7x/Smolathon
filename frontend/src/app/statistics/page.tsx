b4d46a0c2b8d736ee0e847152136264eb354affd
desty <dimonmikryukov@gmail.com>
2025-09-22 18:32:23 +0400

    feat: add mapwidget and statistic page

---
+++ frontend/src/app/statistics/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import MapWidget from "@/app/components/map/MapWidget";
+
+export default function StatisticsPage() {
+  return (
+    <main className="flex flex-col gap-6 p-6">
+      {/* Заголовок */}
+      <header>
+        <h1 className="text-2xl font-bold text-[#62a744]">
+          Статистика дорожного движения
+        </h1>
+        <p className="text-gray-600">
+          На карте показаны ДТП, камеры и светофоры города Смоленска.
+        </p>
+      </header>
+
+      {/* Карта */}
+      <section className="rounded-2xl shadow p-2 border border-gray-200">
+        <h2 className="text-lg font-semibold mb-2">Карта Смоленска</h2>
+        <MapWidget />
+      </section>
+    </main>
+  );
+}
