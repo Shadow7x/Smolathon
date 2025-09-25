832d9c68986105682a735f160288a790ab12d408
desty <dimonmikryukov@gmail.com>
2025-09-26 03:13:38 +0400

    fix: globall css fix & auth page

--- frontend/src/app/news/page.tsx
+++ frontend/src/app/news/page.tsx
@@ -1,6 +1,15 @@
+"use client";
 import MainNewSection from "@/widgets/mainNewSection/mainNewSection";
+import { useEffect } from "react";
 
 const NewsPage = () => {
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      alert("Вы авторизованы");
+    }
+  }, []);
+
   return (
     <div>
       <MainNewSection />
