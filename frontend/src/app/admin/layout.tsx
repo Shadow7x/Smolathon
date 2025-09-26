414fe87b370228e3e3b58dda5fcb5a14357c62d6
HomnixMirn <vitalik.999f9cfe@gmail.com>
2025-09-26 23:04:07 +0400

    наконец есть адаптив

--- frontend/src/app/admin/layout.tsx
+++ frontend/src/app/admin/layout.tsx
@@ -2,11 +2,13 @@ import LeftSidePanel from "@/components/admin/leftSidePanel";
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-
   return (
-    <div className=" flex flex-row">
-        <LeftSidePanel/>
-        {children}
+    <div className="flex min-h-screen">
+      {/* Левая панель */}
+      <LeftSidePanel />
+
+      {/* Контент */}
+      <main className="flex-1">{children}</main>
     </div>
   );
 }
