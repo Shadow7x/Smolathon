3281b5e3ceaf4e94fb36f853c7d981d90744a61c
desty <dimonmikryukov@gmail.com>
2025-09-26 14:49:26 +0400

    fix: remove alert from news

--- frontend/src/app/news/page.tsx
+++ frontend/src/app/news/page.tsx
@@ -3,13 +3,6 @@ import MainNewSection from "@/widgets/mainNewSection/mainNewSection";
 import { useEffect } from "react";
 
 const NewsPage = () => {
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      alert("Вы авторизованы");
-    }
-  }, []);
-
   return (
     <div>
       <MainNewSection />
