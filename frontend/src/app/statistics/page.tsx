cb9fdd1ca86542aa85c220a31f68f404a92769ed
desty <dimonmikryukov@gmail.com>
2025-09-26 15:14:07 +0400

    fix: fix bug in statistic page

--- frontend/src/app/statistics/page.tsx
+++ frontend/src/app/statistics/page.tsx
@@ -3,8 +3,8 @@ import DTPAnalitics from "@/widgets/analiticsSection/DTP/DTPAnalitics";
 
 export default function StatisticsPage() {
   return (
-    <div className="mt-[8rem]">
-      <DTPAnalitics/>
+    <div>
+      <DTPAnalitics />
     </div>
   );
 }
