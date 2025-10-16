5aa17c3217fcc60684f693029d9b7a1f51463671
HomnixMirn <vitalik.999f9cfe@gmail.com>
2025-10-16 17:49:39 +0400

    приёмка

--- frontend/src/app/admin/Map/page.tsx
+++ frontend/src/app/admin/Map/page.tsx
@@ -1,8 +1,10 @@
 "use client"
 import AnaliticsMap from "@/widgets/Map/analiticsMap/analiticsMap"
+import Carsine from "@/widgets/Map/carsine/carsine"
 export default function Map(){
     return(
         <div>
+            <Carsine />
             <AnaliticsMap />
             <h1>MAP</h1>
         </div>
