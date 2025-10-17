133a289f1c11fb87071ceceed3a2b02abcc5e098
Shadow7x <loginvlad1000@gmail.com>
2025-10-17 09:52:58 +0300

    для сервера

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,6 +1,6 @@
-export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/v1/";
-export const MEDIA_URL =
-  process.env.NEXT_PUBLIC_MEDIA_URL || "http://localhost:8000";
-// export const API_URL = process.env.NEXT_PUBLIC_API_URL ||"http://localhost:80/v1/";
-// export const MEDIA_URL = process.env.NEXT_PUBLIC_MEDIA_URL || "http://localhost:80";
+// export const API_URL =
+//   process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/v1/";
+// export const MEDIA_URL =
+//   process.env.NEXT_PUBLIC_MEDIA_URL || "http://localhost:8000";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL ||"http://localhost:80/v1/";
+export const MEDIA_URL = process.env.NEXT_PUBLIC_MEDIA_URL || "http://localhost:80";
